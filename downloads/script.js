7b871ddb17be7b1a0837e7653f27e80362301aed
Siebrex <98038100+Siebrex@users.noreply.github.com>
2022-01-26 18:56:04 -0300

    commit exercises

---
+++ downloads/script.js
@@ -0,0 +1,29 @@
+// o código abaixo escreve alguma mensagem no devtools
+/* aqui eu tenho um comentario de mais de um linha
+para fecha-lo deverá ser colocado os mesmo caracteres do início
+*/
+console.log('Bem vindos ao state!') 
+console.log('jade')
+// na utilizaçao do template poderá ser colocado expressao de limguagens(chamado de interpolação), ex: 
+console.log(`jade ${1 +1}`)
+//nuemros:
+console.log(22 + 12.6)
+console.log(24 * "abcd")
+console.log(12.6 === Infinity)
+//ex:
+console.log(null === undefined)
+//objeto
+console.log({
+    name: "jade",
+    idade: 23,
+    andar: function(){
+        console.log(andar)
+    }
+    //funçao(funcionalidade- dentro dela poderá ser colocado um código console.log 
+})
+//array
+console.log([
+    "leite",
+    "ovos",
+    2,
+])
