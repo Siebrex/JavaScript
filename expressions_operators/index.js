275d86d1acaa2afc1bc06d064cee75cb92403131
Siebrex <98038100+Siebrex@users.noreply.github.com>
2023-02-09 08:29:19 -0300

    commit to expressions_operators
    commit changes

--- expressions_operators/index.js
+++ expressions_operators/index.js
@@ -33,4 +33,47 @@ delete person.city
 
 console.log(person)
 
+//remainder from the division(resto da divisao)
+let remainder
+remainder = 11 % 9
+console.log(remainder)
 
+//increment ++ increment always will be the sum of one more in the number.
+let increment = 2
+increment++
+console.log(++increment);/* se colocar detro do console, porem depois do nome, sera incrementado depois. se for colocado antes do nome, sera invcrementado antes */
+
+//decrement --
+let decrement = 0
+decrement--
+console.log(decrement);
+
+//expotencial **
+console.log(2**3);
+
+//Grouping operator () - operator that will agroup expressions. 
+
+let calculo = (3+5) *5
+console.log(calculo);
+
+//comparison operators- i will compare values and return Boolean as answer of the comparison.
+
+let one = 1
+let two = 2
+
+// == igual a 
+
+console.log(two == 1);
+console.log(one == 1);  
+
+//!= different than 
+
+console.log(two !=2);
+console.log(one !=2);
+
+/*if you want to compare teh values and the tipe, you have to use "===", it will be called "estritamente igual a". The same thing for different "!=="*/
+
+let jade = 3 
+
+console.log( jade === "3"); // string 
+console.log(jade === 3); //number
